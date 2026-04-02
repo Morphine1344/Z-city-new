4ce9600ee51d88d2f3e25100d2faa0917b207bd4
Morphine <moprhine1344@gmail.com>
2026-04-02 16:15:35 +0300

    refactor(bloodz): bloodz playerclass now in OOP
    * Feature: subclasses OG, Big, Lil
    * Feature: random bodygroups

--- lua/homigrad/playerclass/classes/sh_bloodz.lua
+++ lua/homigrad/playerclass/classes/sh_bloodz.lua
@@ -1,40 +1,91 @@
-local CLASS = player.RegClass("bloodz")
+--- @class BloodzClass : PlayerClass
+local BloodzClass = hg.PlayerClass:Extend({
+    name = "bloodz",
+    accessories = false,
+    color = {
+        red = 165,
+        green = 0,
+        blue = 0
+    },
+    subclasses = {
+        OG = {
+            prefixes = {
+                OG = 100
+            },
+            chance = 5,
+            models = {
+                "models/gang_ballas_boss/gang_ballas_boss.mdl"
+            }
+        },
+        Big = {
+            prefixes = {
+                Big = 100
+            },
+            chance = 20,
+            models = {
+                "models/gang_ballas_chem/gang_ballas_chem.mdl"
+            }
+        },
+        Lil = {
+            prefixes = {
+                Lil = 100
+            },
+            chance = 75,
+            models = {
+                "models/gang_ballas/gang_ballas_1.mdl",
+            }
+        }
+    }
+})
 
-function CLASS.Off(self)
-    if CLIENT then return end
+function BloodzClass:On(ply)
+    if CLIENT then
+        return
+    end
+
+    self:SetSubclassesBodygroups({
+        OG = {
+            bodygroups = {
+                glasses = math.random(0, 1),
+                rings = math.random(0, 1),
+                chain = math.random(0, 1)
+            }
+        },
+        Big = {
+            bodygroups = {
+                cap = math.random(0, 2),
+                mask = math.random(0, 1),
+                arms = math.random(0, 1),
+                arm_clock = math.random(0, 1),
+            }
+        },
+        Lil = {
+            bodygroups = {
+                cap = math.random(0, 1),
+                body = math.random(0, 2),
+                glasses = math.random(0, 2),
+                mask = math.random(0, 2),
+                arms = math.random(0, 2),
+                arm_clock = math.random(0, 1),
+            }
+        }
+    })
+
+    local subclass = self:SetSubclass()
+
+    self:SetAppearance(ply, {
+        subMaterial = false
+    })
+    self:SetMdl(ply)
+    self:SetupBodygroups(ply, {
+        bodygroups = subclass.bodygroups
+    })
+    self:SetColor(ply)
+    self:SetName(ply)
 end
 
-local models = {
-    "models/gang_ballas_chem/gang_ballas_chem.mdl",
-    "models/gang_ballas/gang_ballas_2.mdl",
-    "models/gang_ballas/gang_ballas_1.mdl"
-}
-
-local subnames = {
-	"Big ",
-	"Lil ",
-	"OG "
-}
-
-function CLASS.On(self)
-    if CLIENT then return end
-    ApplyAppearance(self,nil,nil,nil,true)
-    local Appearance = self.CurAppearance or hg.Appearance.GetRandomAppearance()
-    Appearance.AAttachments = ""
-    Appearance.AColthes = ""
-    self:SetNWString("PlayerName",subnames[math.random(#subnames)] .. Appearance.AName)
-    self:SetPlayerColor(Color(165,0,0):ToVector())
-    self:SetModel(models[math.random(#models)])
-	for _, bg in ipairs(self:GetBodyGroups()) do
-		self:SetBodygroup(bg.id, math.random(0, bg.num))
-	end
-
-    local inv = self:GetNetVar("Inventory", {})
-    inv["Weapons"] = inv["Weapons"] or {}
-    inv["Weapons"]["hg_sling"] = true
-    self:SetNetVar("Inventory", inv)
-
-    self:SetSubMaterial()
-    
-    self.CurAppearance = Appearance
+function BloodzClass:Off()
+    if CLIENT then
+        return
+    end
 end
